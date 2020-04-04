4436d806c6ea40c93e4abeb8b208a62234ba7d4b
Szymon Nowicki <sznowicki@gmail.com>
2020-04-04 20:35:17 +0200

    Correct fingerprint

--- dkr.rb
+++ dkr.rb
@@ -2,7 +2,7 @@ class Dkr < Formula
   desc "Dkr is a command line (CLI) tool that is a drop-in solution for `docker` and `docker-compose` tools."
   homepage "https://github.com/ohdkr/dkr"
   url "https://github.com/ohdkr/dkr/releases/download/v0.0.1/dkr_0.0.1_Darwin_x86_64.tar.gz"
-  sha256 "02a90c452e1263c1b0b4f40caa0282910784eb3b9c428e89bebd1ffe20ef00a8"
+  sha256 "1211797212c3660dfe506952ea96bf722b0c09821c9d8206291d02eeed8b19f2"
   version "0.0.1"
 
   bottle :unneeded
