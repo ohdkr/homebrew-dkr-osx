b942a8074dcbb1cf798634ee115a6c4cb39edf0f
Szymon Nowicki <sznowicki@gmail.com>
2020-04-04 23:24:42 +0200

    v0.1.0

--- dkr.rb
+++ dkr.rb
@@ -1,9 +1,9 @@
 class Dkr < Formula
   desc "Dkr is a command line (CLI) tool that is a drop-in solution for `docker` and `docker-compose` tools."
   homepage "https://github.com/ohdkr/dkr"
-  url "https://github.com/ohdkr/dkr/releases/download/v0.0.1/dkr_0.0.1_Darwin_x86_64.tar.gz"
-  sha256 "1211797212c3660dfe506952ea96bf722b0c09821c9d8206291d02eeed8b19f2"
-  version "0.0.1"
+  url "https://github.com/ohdkr/dkr/releases/download/v0.1.0/dkr_0.1.0_Darwin_x86_64.tar.gz"
+  sha256 "8b6b98945ea9508170b8f3df861561b15078ffaa80d5780e01fc8ecd25126287"
+  version "0.1.0"
 
   bottle :unneeded
 
