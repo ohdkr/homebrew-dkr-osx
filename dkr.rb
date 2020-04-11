79ccda1dc78cfe617b10ade3a3da2dcf6e45d051
Szymon Nowicki <sznowicki@gmail.com>
2020-04-11 20:28:45 +0200

    v0.2.0 - correct files

--- dkr.rb
+++ dkr.rb
@@ -1,8 +1,8 @@
 class Dkr < Formula
   desc "Dkr is a command line (CLI) tool that is a drop-in solution for `docker` and `docker-compose` tools."
   homepage "https://github.com/ohdkr/dkr"
-  url "https://github.com/ohdkr/dkr/releases/download/0.2.0/dkr_0.2.0_Linux_x86_64.tar.gz"
-  sha256 "21c4d96327fa408abf9086d8fead5fd0d17a7ffba1509e37067835fde93d1827"
+  url "https://github.com/ohdkr/dkr/releases/download/0.2.0/dkr_0.2.0_Darwin_x86_64.tar.gz"
+  sha256 "4b464fe5b65ae59f783b9355c92a259365f2706e8c4f6707e695616101ea1880"
   version "0.2.0"
 
   bottle :unneeded
